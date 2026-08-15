fff1c2e437b3abad54ddbd2212f5eb504619b3b6
Manan Chahal <mananchahal01@gmail.com>
2026-08-15 23:46:22 +0530

    Assignment 2 and week 4

--- UCI Heart Disease/assignment2.r
+++
@@ -1,7 +0,0 @@
-print(getwd())
-# Intilizing the location to load the data
-setwd("D:\\DOCS\\VIT Ebooks\\Fourth Year\\Sem 7\\R\\Lab\\UCI Heart Disease")
-data = read.csv("./heart disease/processed.cleveland.data",header=FALSE)
-
-# Chaning the headers
-colnames(data) <- c("age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num")
